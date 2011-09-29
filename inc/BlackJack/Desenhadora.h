93ad4e51162663c7640cf45870e112d9257e4a12
catharinequintans <catharinequintans@localhost>
2011-09-29 00:41:24 -0300

    Desenhadora

---
+++ inc/BlackJack/Desenhadora.h
@@ -0,0 +1,16 @@
+/*
+ * Desenhadora.h
+ * Created on: 29/09/2011
+ * Author: Catharine
+ */
+
+#ifndef DESENHADORA_H_
+#define DESENHADORA_H_
+
+class Desenhadora {
+public:
+	Desenhadora();
+	virtual ~Desenhadora();
+};
+
+#endif /* DESENHADORA_H_ */
